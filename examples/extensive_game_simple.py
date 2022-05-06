2366636d8fadb005a0b5000b5446dbb6fe7072e0
Martin Le <le@ifn.ing.tu-bs.de>
2022-05-07 01:49:19 +0200

    Add more simple examples for extensive game

--- examples/extensive_game_simple.py
+++ examples/extensive_game_simple.py
@@ -23,25 +23,30 @@ move.actions[0].label = 'F'
 move.actions[1].label = 'G'
 
 payoff_tuple = g.outcomes.add("5,2")
-payoff_tuple[0] = 5
-payoff_tuple[1] = 2
+payoff_tuple[0] = 1
+payoff_tuple[1] = 1
 g.root.children[0].children[0].outcome = payoff_tuple
 
 payoff_tuple = g.outcomes.add("3,1")
-payoff_tuple[0] = 3
+payoff_tuple[0] = 1
 payoff_tuple[1] = 1
 g.root.children[0].children[1].outcome = payoff_tuple
 
 payoff_tuple = g.outcomes.add("6,3")
-payoff_tuple[0] = 6
-payoff_tuple[1] = 3
+payoff_tuple[0] = 1
+payoff_tuple[1] = 1
 g.root.children[1].children[0].outcome = payoff_tuple
 
 payoff_tuple = g.outcomes.add("4,4")
-payoff_tuple[0] = 4
-payoff_tuple[1] = 4
+payoff_tuple[0] = 1
+payoff_tuple[1] = 1
 g.root.children[1].children[1].outcome = payoff_tuple
 
-solver = pygambit.nash.ExternalLCPSolver()
+solver = pygambit.nash.ExternalEnumPureSolver()
 nash_eq = solver.solve(g)
-print(nash_eq)
+print(nash_eq[0]._profile)
+print(nash_eq[1]._profile)
+
+# Write game into a file
+with open("extensive_game_simple_test.efg", "w") as f:
+    f.write(g.write())
